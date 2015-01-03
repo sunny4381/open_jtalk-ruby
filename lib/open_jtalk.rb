6c07825c3475caea4f9c75c23891db128de035ca
NAKANO Hideo <pinarello.marvel@gmail.com>
2015-01-03 11:17:15 +0900

    add support lame load error.

--- lib/open_jtalk.rb
+++ lib/open_jtalk.rb
@@ -1,7 +1,10 @@
-require 'lame'
+require 'open_jtalk/config'
+require 'open_jtalk/version'
+require 'open_jtalk/open_jtalk'
+require 'open_jtalk/wave_file_writer'
 
-require "open_jtalk/config"
-require "open_jtalk/version"
-require "open_jtalk/open_jtalk"
-require "open_jtalk/wave_file_writer"
-require "open_jtalk/mp3_file_writer"
+begin
+  require 'lame'
+  require 'open_jtalk/mp3_file_writer'
+rescue
+end
